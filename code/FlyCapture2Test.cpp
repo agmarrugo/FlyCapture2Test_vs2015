2eab10b482a9f071f7dd4b6cf9156bb515ec685d
Andres Marrugo <agmarrugo@gmail.com>
2020-01-24 11:11:10 -0500

    Update FlyCapture2Test.cpp

--- code/FlyCapture2Test.cpp
+++ code/FlyCapture2Test.cpp
@@ -1,21 +1,9 @@
 //=============================================================================
-// Copyright © 2017 FLIR Integrated Imaging Solutions, Inc. All Rights Reserved.
+// This software is partially based on FlyCapture2Test.cpp by FLIR
 //
-// This software is the confidential and proprietary information of FLIR
-// Integrated Imaging Solutions, Inc. ("Confidential Information"). You
-// shall not disclose such Confidential Information and shall use it only in
-// accordance with the terms of the license agreement you entered into
-// with FLIR Integrated Imaging Solutions, Inc. (FLIR).
 //
-// FLIR MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
-// SOFTWARE, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-// PURPOSE, OR NON-INFRINGEMENT. FLIR SHALL NOT BE LIABLE FOR ANY DAMAGES
-// SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
-// THIS SOFTWARE OR ITS DERIVATIVES.
+// by professor Andres Marrugo, 2020 - Universidad Tecnologica de Bolivar, Colombia
 //=============================================================================
-//=============================================================================
-// $Id: FlyCapture2Test.cpp 316528 2017-02-22 00:03:53Z alin $
 //=============================================================================
 
 #include "stdafx.h"
